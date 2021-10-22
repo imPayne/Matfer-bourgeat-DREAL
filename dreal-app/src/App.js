5c737e51cf57ea16c8550ee82022f8be8c9ce118
monchablon hugo <mhugo@localhost.localdomain>
2021-10-22 15:59:23 +0200

    added canvas rect test

--- dreal-app/src/App.js
+++ dreal-app/src/App.js
@@ -1,19 +1,26 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import ReactDOM from "react-dom";
 
-class App extends React.Component {
-    render() {
-        const title = "Application de visualisation et de localisation des stocks";
-        //const ......
-        return (
-            <div>
-                <h1>{title}</h1>
-            </div>
-        )
-    }
+const Canvas = props => {
+    const canvasRef = useRef(null)
+
+    useEffect(() => {
+        const canvas = canvasRef.current
+        const context = canvas.getContext('2d')
+        let width = context.canvas.width - 10;
+        let height = context.canvas.height - 10;
+        context.fillStyle = '#000000'
+        context.fillRect(0, 0, width, height)
+        context.clearRect(5, 5, width, height);
+    }, [])
+
+    return <div>
+        <h1>Visualisation et localisation des stocks</h1>
+        <canvas ref={canvasRef} {...props}/>
+        </div>
 }
 
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<Canvas />, rootElement);
 
-export default App;
+export default Canvas

58b4b33f59a0bd59b4165d1962b6b68d974eefc6
monchablon hugo <mhugo@localhost.localdomain>
2021-11-18 10:29:23 +0100

    add features

--- test_canvas/API/server.js
+++ test_canvas/API/server.js
@@ -5,7 +5,6 @@ const csvFile = "../csv/storages.csv";
 const mysql = require('mysql');
 const csv = require('csv-parser');
 const fs = require('fs');
-const { CLIENT_RENEG_WINDOW } = require('tls');
 
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -44,7 +43,6 @@ class Zone extends Area {
       this.alley = alley;
       this.column = column;
   }
-  // faire les Get manquants
   addStorage(storage) {
     var index = this.storages.findIndex(function (s) {
       return(s.level === storage.level && s.storage === storage.storage);
@@ -68,59 +66,100 @@ class Storage {
 
 function GetPreciseLocationY(row) {
   var alleyNumber = +row.alley.match(/\d+/g);//.join('');
-  //console.log(row.alley + " = " + alleyNumber);
   if (alleyNumber % 2 == 0) {
     var preciseY = parseFloat(row.Y, 10) + 20;
-    //console.log("La je additionne car " + alleyNumber + " est pair donc " + row.Y + " + 20 = " + preciseY);
   }
   else {
     var preciseY = parseFloat(row.Y, 10) - 20;
-    //console.log("La je soustrait car " + alleyNumber + " est impair donc " + row.Y + " - 20 = " + preciseY);
   }
   return(preciseY);
 }
+
+function getBuildingId(row) {
+  // building id return, il y a 4 buildings soit 4 id différent.
+  var mainBuilding = 1;
+  if (row.buildings == "FLO") {
+    return(mainBuilding);
+  }
+}
+
+//sendDataBuildings();
+
+
+function sendDataBuildings() {
+  //permet d'initialiser la table Buildings dans la DB
+  var query = "INSERT INTO Buildings SET ?";
+  var array = [
+    {building_id: 1,posX: 90, posY: 50, width: 2315, height: 1040},
+    {building_id: 2,posX: 60, posY: 1150, width: 460, height: 530},
+    {building_id: 3,posX: 640, posY: 1150, width: 570, height: 590},
+    {building_id: 4,posX: 1335, posY: 1150, width: 405, height: 430},
+  ]
+
+  for (i in array) {
+    var params = {building_id: array[i].building_id, posX: array[i].posX, posY: array[i].posY, width: array[i].width, height: array[i].height};
+    connection.query(query, params);
+  }
+  console.log("Insert in Buildings table success !");
+}
+
+function deleteOldData() {
+  var queryDeleteZone = "DELETE FROM `epytodo`.`Zone` WHERE (`id` >= '0');"
+  var queryDeleteStorage = "DELETE FROM `epytodo`.`Storage` WHERE (`id` >= '0');"
+  
+  connection.query(queryDeleteStorage);
+  connection.query(queryDeleteZone);
+  console.log("Delete Success !");
+}
+
+deleteOldData();
 
 
 fs.createReadStream(csvFile)
   .pipe(csv())
   .on("data", (row) => {
-
-
+    //create buildings in Buildings table
     var preciseY =  GetPreciseLocationY(row);
     var rowX = parseInt(row.X, 10);
-    //console.log("Zone(row.X = " + rowX + ", preciseY = " + preciseY + ", 20, 20, row.alley = " + row.alley + ", row.column = " + row.column);
+    row.column = '' ? 'none' : row.column;
     var zone = new Zone(rowX, preciseY, 20, 20, [], row.alley, row.column);
     var storage = new Storage(row.level, row.storage, 0, 0, 0, 0, 0);
-    var paramsZone = {zone_posX: rowX, zone_posY: preciseY, zone_width: 20, zone_height: 20};
+
+    var buildingNumberId = getBuildingId(row);
+    
+
+    //set prerequis query
+    var paramsZone = {building_id: buildingNumberId,zone_posX: rowX, zone_posY: preciseY, zone_width: 20, zone_height: 20, alley: row.alley, column: row.column};
     var queryZone = "INSERT INTO Zone SET ?";
     var queryStorage = "INSERT INTO Storage SET ?";
-    var paramsStorage = {level: row.level, storage: row.storage};
+    var storageZoneId;
 
-
-    //console.log("Params(zone_posX = " + zone._posX + ", zone_posY = " + zone._posY);
     var index = zones.findIndex(function (z) {
-      //creer un storage si pas deja créer(le storage)
       return z.alley === zone.alley && z.column === zone.column;
     });
     //si index pas trouvé (donc output == -1) => on push ds zones
     if (index == -1) {
-      //creer un storage si pas deja créer(le storage)
       zone.addStorage(storage);
-      zones.push(zone)
+      zones.push(zone);
+      connection.query(queryZone, paramsZone);
     }
     else {
       zones[index].addStorage(storage);
     }
-    console.log("number = " + row.number + " | row.level = " + row.level + " | row.storage = " + row.storage);
-    //Insert en base
-    connection.query(queryStorage, paramsStorage);
-    //connection.query(queryZone, paramsZone);
-  })
-  .on("end", () => {
-    console.log("CSV file successfully processed");
-    //console.log(zones);
-  });
+    connection.query("SELECT `id` FROM Zone WHERE ?", [{alley: row.alley}, {column: row.column}], function(err, result, fields) {
 
+      if (err) throw err;
+        storageZoneId = result[0].id;
+        console.log(storageZoneId);
+    }); //récupère l'id Zone grâce à l'alley et le current alley 
+    console.log(storageZoneId);
+    var paramsStorage = {zone_id: storageZoneId,level: row.level, storage: row.storage};
+    connection.query(queryStorage, paramsStorage);
+  })
+  
+  .on("end", () => {
+    console.log("CSV file successfully processed and Data inserted in Zone and Storage");
+  });
 
 app.get("/", (req, res) => {
   res.json({message: 'Root page ready'})

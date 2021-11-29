c052fd4e855e99b978909b2984351457a72221c2
monchablon hugo <mhugo@localhost.localdomain>
2021-11-29 11:00:06 +0100

    Update Database file done the db build isoké

--- php_dreal/Dreal/app/Console/Commands/UpdateDatabase.php
+++ php_dreal/Dreal/app/Console/Commands/UpdateDatabase.php
@@ -41,8 +41,10 @@ class UpdateDatabase extends Command
 
     public function getPreciseY($alley, $posY) {
         $alleyNumber = preg_match('/\d+/', $alley, $matches);
-        if (!$matches)
-            return;
+        if (!$matches) {
+            $defaultY = intval($posY);
+            return($defaultY);
+        }
         $value = intval($matches[0]);
         if ($value % 2 == 0) {
             $preciseY = (int)$posY + 20;
@@ -50,7 +52,6 @@ class UpdateDatabase extends Command
         else {
             $preciseY = (int)$posY - 20;
         }
-        #dd($preciseY);
         return($preciseY);
     }
 
@@ -58,19 +59,19 @@ class UpdateDatabase extends Command
     {
         //posX,posY,width,height
         // insert buildings data in database by parsing a csv file
-        if (($handle = fopen(public_path(). '/buildings.csv', "r")) !== FALSE) {
-            while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-                $building = new Building();
-                $building->posX = $data[0];
-                $building->posY = $data[1];
-                $building->width = $data[2];
-                $building->height = $data[3];
-                $building->save();
-            }
+        $building = Building::findOrNew(1);
+        if (!$building->id) {
+            $building->id = 1;
+            $building->posX = 90;
+            $building->posY = 50;
+            $building->width = 2315;
+            $building->height = 1040;
+            $building->save();
         }
+
         //number,alley,column,level,storage,buildings,X,Y
         //parsing CSV file line per line and create new storage and assign data
-        if (($handle = fopen(public_path(). '/storages.csv', "r")) !== FALSE) {
+        if (($handle = fopen(public_path(). '/test.csv', "r")) !== FALSE) {
             while (($data = fgetcsv($handle, 10000, ",")) !== FALSE) {
                 $storage = new Storage();
                 $storage->number = $data[0];
@@ -87,13 +88,15 @@ class UpdateDatabase extends Command
                     $zone->width = 20;
                     $zone->height = 20;
                     $zone->posY = $this->getPreciseY($data[1], $data[7]);
+                    $zone->building_id = 1;
                     $zone->save();
                     $storage->zone()->associate($zone->id);
                 }
                 else {
                     $storage->zone()->associate($zoneExist->id);
                 }
-                $zone->building()->associate($building->id);
+
+                $zoneExist->building()->associate($building->id);
                 $storage->save();
             }
             fclose($handle);
---
+++ php_dreal/Dreal/resources/views/components/show-building-list.blade.php
@@ -0,0 +1,3 @@
+<div>
+    <h1>Liste des Buildings</h1>
+</div>
